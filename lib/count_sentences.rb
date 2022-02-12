8a1ef1701a5ac872b9f123194e993abbfbe3ba10
Jesse Brown <Brownjesse8895@yahoo.com>
2022-02-12 14:05:08 -0600

    commit tree 749fd20ffbbc53054c03f4359a144cf8287b0e10

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+   sentence = 0
+   
+    punctuations = [".", "!", "?"]
+    
+    self.chars.each_with_index do |char, index | 
+      
+     
+      if punctuations.include?(char) && !punctuations.include?(self[index + 1])
+        
+      # Go through each iteration and check to see if the char matches one of the punctuations
+      # Once we determine there is a match increment count by 1 
+        sentence += 1
+      end
+    end 
+    sentence
   end
 end
+
+
+      
+
